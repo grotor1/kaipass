8e753f6f84a1311ea0be63d13af9ea2f8b3e416a
grotor <varukhin04@mail.ru>
2020-10-11 05:38:49 +0300

    Updates

---
+++ precache-manifest.a10c30539da6d6dbe8157415fd8ba371.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf2339a854b7abd77a6f731db3884ab",
+    "url": "/kaipass/index.html"
+  },
+  {
+    "revision": "be215c43fcaa59b1817f",
+    "url": "/kaipass/static/css/main.459d0bd2.chunk.css"
+  },
+  {
+    "revision": "b5710f33debcd4355f38",
+    "url": "/kaipass/static/js/2.10abca65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kaipass/static/js/2.10abca65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be215c43fcaa59b1817f",
+    "url": "/kaipass/static/js/main.aced0e56.chunk.js"
+  },
+  {
+    "revision": "dcfedfa787f0195c491a",
+    "url": "/kaipass/static/js/runtime-main.62559329.js"
+  },
+  {
+    "revision": "5b473fe24ce825c957c4f7a0270d39ec",
+    "url": "/kaipass/static/media/background_fudzi.5b473fe2.png"
+  },
+  {
+    "revision": "2f5bb78f74f91fb47838e30114f22d37",
+    "url": "/kaipass/static/media/eye-close.2f5bb78f.svg"
+  },
+  {
+    "revision": "9414bddcdd0879003767fb8546d29ebc",
+    "url": "/kaipass/static/media/eye-open.9414bddc.svg"
+  },
+  {
+    "revision": "026741308b5c8ffef2b3ead81a2ffaf5",
+    "url": "/kaipass/static/media/facebook.02674130.svg"
+  },
+  {
+    "revision": "dd65d12d7640bd3c9106c934cc8edb7c",
+    "url": "/kaipass/static/media/google-plus 1.dd65d12d.svg"
+  },
+  {
+    "revision": "a2954d4f119a78c46b2d2cbb32f28fae",
+    "url": "/kaipass/static/media/right-arrow.a2954d4f.svg"
+  },
+  {
+    "revision": "d83893cd077da2ed711893fc0470e79f",
+    "url": "/kaipass/static/media/svg-wave_pink.d83893cd.svg"
+  },
+  {
+    "revision": "a0980c0817374e298f0bf40d04af9d29",
+    "url": "/kaipass/static/media/svg-wave_white.a0980c08.svg"
+  }
+]);
