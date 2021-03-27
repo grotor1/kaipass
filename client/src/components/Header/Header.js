01eb9d9d6a7bbfcdc1de0f29390e1a0a2d86c425
Vladimir Varukhin <varukhin04@mail.ru>
2021-03-27 08:56:49 +0300

    logout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import './Header.css'
-import React from 'react'
+import React, {useContext} from 'react'
+import {AuthContext} from "../../context/AuthContext";
 
 
 const Header = () => {
+    const auth = useContext(AuthContext)
     return (
         <div className="header">
             <div className='header__search-block'/>
-            <div className="header__notifications-button"/>
+            <div className="header__notifications-button" onClick={auth.logout}/>
         </div>
     )
 }
